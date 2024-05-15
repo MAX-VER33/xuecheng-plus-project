3ec0c937e68bdd65ad4570ac734bc509aace57aa
MAX-VER33 <3021036202@qq.com>
2024-05-15 18:59:32 +0800

    5.15

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TeachplanController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TeachplanController.java
@@ -1,12 +1,13 @@
 package com.xuecheng.content.api;
 
+import com.xuecheng.content.model.dto.SaveTeachplanDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.service.TeachplanService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,12 +15,24 @@ import java.util.List;
 @RestController
 public class TeachplanController {
 
+    @Autowired
+    TeachplanService teachplanService;
+
     @ApiOperation("查询课程计划树形结构")
     @ApiImplicitParam(value = "courseId",name = "课程Id",required = true,dataType = "Long",paramType = "path")
     @GetMapping("/teachplan/{courseId}/tree-nodes")
     public List<TeachplanDto> getTreeNodes(@PathVariable Long courseId){
-        return null;
+        List<TeachplanDto> teachplanTree=teachplanService.findTeachplanTree(courseId);
+        return teachplanTree;
     }
+
+    @ApiOperation("课程计划创建或修改")
+    @PostMapping("/teachplan")
+    public void saveTeachplan( @RequestBody SaveTeachplanDto teachplan){
+teachplanService.saveTeachplan(teachplan);
+    }
+
+
 
 }
 
